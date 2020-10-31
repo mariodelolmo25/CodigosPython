20a2050b8cf5777b5375fa46cb2036a37960b34e
mariodelolmo25 <mariodelolmo25@users.noreply.github.com>
2020-10-31 21:20:28 +0000

    Create NEGATIVOS.py

---
+++ ProyectosMario/NEGATIVOS.py
@@ -0,0 +1,18 @@
+# _-_ coding: utf8 _-_
+
+#__INICIO
+print("PRUEBA NEGATIVOS")
+#Input números
+numero1 = float(input("Introducir primer valor: "))
+numero2 = float(input("Introducir el segundo valor: "))
+print("\n")
+print(f"Valores introducidos (por orden): ({numero1}); ({numero2})\n")
+
+
+if numero1 < 0:
+  numero1 = print(f"({numero1})")
+if numero2 < 0:
+  numero2 = print(f"({numero2})")
+
+
+print ("\t""fin")
