7b2d19971cba3a9f623195dd432eaae83cd83c10
Mario del Olmo <62056104+mariodelolmo25@users.noreply.github.com>
2020-11-01 16:04:34 +0000

    Calculadora de dos valores v2.0
    CAMBIOS VERSIÓN 1.2:
       Ahora los números negativos se mostrarán entre paréntesis.
       Se han corregido algunos errores gramaticales.
       El nombre de cada operación se representa en mayúsculas.
       Se ha eliminado una parte del código inservible (antes desactivada en comentario)
       Ahora la suma, resta y multiplicaión muestran dos cifras decimales en vez de tres.

--- ProyectosMario/Calculadora de dos números v1.0.py
+++ ProyectosMario/Calculadora de dos números v1.0.py
@@ -1,77 +1,75 @@
-#NOTAS DE VERSIÓN 1.1:
-  #Línea 77: ahora divide entre numero2div. Antes entre numero2
-
-
 # _-_ coding: utf8 _-_
 #__INICIO
-print("Bienvenido a la calculadora de dos valores. Versión: 1.1")
+print ("Autor: Mario del Olmo [Versión: 1.2]\n")
+print("Bienvenido a la calculadora de dos valores.")
 #Input números
-numero1 = float(input("Introducir primer valor: "))
-numero2 = float(input("Introducir el segundo valor: "))
+numero1Raw = float(input("Introducir primer valor: "))
+numero2Raw = float(input("Introducir el segundo valor: "))
 print ("") #intro
-diffNum = numero2 - numero1 #no se usa
+diffNum = numero2Raw - numero1Raw #no se usa
 #__COMPROBACIÓN DE NÚMEROS
-
+if numero1Raw < 0:
+  numero1Neg = str(f"({numero1Raw})")
+  print (numero1Neg)
+else:
+  numero1Neg = numero1Raw
+if numero2Raw < 0:
+  numero2Neg = str(f"({numero2Raw})")
+  print (numero2Neg)
+else:
+  numero2Neg = numero2Raw
 #intentar hacer que en los negativos te los ponga entre paréntesis
-
 def comprobaciónNúmeros():
-  menorigualcero = numero1 + numero2
+  menorigualcero = numero1Raw + numero2Raw
   if menorigualcero <= 0:
     print ("  AVISO:\n")
     print(f"\tLa suma de ambos números es: {menorigualcero}")
     print("\tTen en cuenta que estás trabajando con números negativos, el resultado puede alterarse.""\n")
-  if numero2 > numero1:
-    print(f"\tEl segundo número es mayor al primero, tenlo en cuenta para la división entera.""\n")
+  if numero1Raw > numero2Raw:
+    print(f"\tEl primer número es mayor al primero, tenlo en cuenta para la división entera.""\n")
 comprobaciónNúmeros()
-if numero2 == 0:
+if numero2Raw == 0:
   numero2div = 1
 else:
-  numero2div = numero2 
+  numero2div = numero2Raw 
 print ("") #intro
-print (f"Valores introducidos (por orden): ({numero1}); ({numero2})")
-comprobación = (f"Valores introducidos (por orden): {(numero1)}; {(numero2)}")
+print (f"Valores introducidos (por orden): ({numero1Raw}); ({numero2Raw})")
+comprobación = (f"Valores introducidos (por orden): {(numero1Raw)}; {(numero2Raw)}")
 # Ajuste de Valores
-
 #__FORMATO VALORES
-
 # Intentar def format
 # A lo bruto
-suma = numero1 + numero2
-resta = numero1 - numero2
-if numero2 != 0:
-  división = numero1 / numero2
+suma = numero1Raw + numero2Raw
+resta = numero1Raw - numero2Raw
+if numero2Raw != 0:
+  división = numero1Raw / numero2Raw
 else: 
-  división = (numero1 / (numero2 + numero2div))
-multiplicación = numero1 * numero2
-formatSuma = format(suma, ".3f")
-formatResta = format(resta, ".3f")
-formatMultiplicación = format(multiplicación, ".3f")
+  división = (numero1Raw / (numero2Raw + numero2div))
+multiplicación = numero1Raw * numero2Raw
+formatSuma = format(suma, ".2f")
+formatResta = format(resta, ".2f")
+formatMultiplicación = format(multiplicación, ".2f")
 formatDivisión = format(división, ".3f")
-# Intento de def
-'''
-def formatValores():
-  suma = numero1 + numero2
-  resta = numero1 - numero2
-  división = numero1 / numero2
-  multiplicación = numero1 * numero2
-  formatSuma = format(suma, ".3f")
-  formatResta = format(resta, ".3f")
-  formatMultiplicación = format(multiplicación, ".3f")
-  formatDivision = format(división, ".3f")
-
-formatValores()
-'''
 #__GIONES DE SEPARACIÓN
 longComprobación = len(comprobación)
 print ((5 + longComprobación) * "-")
 #__MUESTRA OPERACIONES FORMATEADAS
-print("OPERACIONES: \n")
-print("\t" "Suma:", (numero1), "+", numero2, "=", formatSuma)
-print("\t" "Resta:", numero1, "-", "(",numero2,")", "=", formatResta)
-print("\t" "Multiplicación:", numero1, "x", numero2, "=", formatMultiplicación)
-if numero2 == 0:
+print ("OPERACIONES: \n")
+print ("\t" "SUMA:", (numero1Neg), "+", numero2Neg, "=", formatSuma)
+print ("\t" "RESTA:", numero1Neg, "-", numero2Neg, "=", formatResta)
+print ("\t" "MULTIPLICACIÓN:", numero1Neg, "x", numero2Neg, "=", formatMultiplicación)
+if numero2Raw == 0:
   print ("")
   print ("ADVERTENCIA: No se puede dividir entre 0\n")
 else:
-  print("\t" "División:", numero1, "/", numero2, "=", formatDivisión)
-  print("\t""División entera:", numero1, "//", numero2, "=", numero1 // numero2)
+  print ("\t" "DIVISIÓN:", numero1Neg, "/", numero2Neg, "=", formatDivisión)
+  print ("\t""DIVISIÓN ENTERA:", numero1Neg, "//", numero2Neg, "=", numero1Raw // numero2div)
+
+  #CAMBIOS VERSIÓN 1.2:
+    # Ahora los números negativos se mostrarán entre paréntesis.
+    # Se han corregido algunos errores gramaticales.
+    # El nombre de cada operación se representa en mayúsculas.
+    # Se ha eliminado una parte del código inservible (antes desactivada en comentario)
+    # Ahora la suma, resta y multiplicaión muestran dos cifras decimales en vez de tres.
+ 
+ # FIN DEL PROGRAMA
