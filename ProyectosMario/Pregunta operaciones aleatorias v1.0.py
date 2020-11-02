53aa4ad388e8f00da0104a1c10092f8c97ee4ab7
Mario del Olmo <62056104+mariodelolmo25@users.noreply.github.com>
2020-11-02 18:24:24 +0000

    [Versión 1.0]

--- ProyectosMario/Pregunta operaciones aleatorias v1.0.py
+++ ProyectosMario/Pregunta operaciones aleatorias v1.0.py
@@ -1,6 +1,6 @@
 #_-_ coding: utf8 _-_
 #__INICIO
-  #Autor e imports
+#Autor e imports
 print ("Autor: Mario del Olmo [Versión: 0.2]\n")
 print ("Bienvenido al programa de cálculo.\n")
 
@@ -8,7 +8,7 @@ import random
 import time
 from colorama import init, Fore, Back, Style
 
-  # Instrucciones visuales
+# Instrucciones visuales
 inicio = 0
 print ("El programa preguntará un rango de valores el cuál servirá para ubicar los valores.")
 print ("El tipo de aporaciones serán: SUMAS, RESTAS y MULTIPLICACIONES")
@@ -19,24 +19,18 @@ input()
 print ((30 * "-"),"\n")
 # DECLARACIÓN DE VARIABLES
 # Globalización de variables (implementar en cada funcion)
-  # .
-  # .
-global num1Procesado
-global num2Procesado
 global resultado
 global respuesta
 global aciertos
 global fallos
 global nota
-global iniciolRango
+global inicioRango
 global finalRango
-global segundos
+
 # Asignación de valores INICIALES
 if inicio == 0:
   num1Raw = int(0)
   num2Raw = int(0)
-  num1Procesado = int(0)
-  num2Procesado = int(0)
   resultado = int(0)
   respuesta = int(0)
   aciertos = int(0)
@@ -44,66 +38,50 @@ if inicio == 0:
   nota = int(0)
   inicioRango = int(0)
   finalRango = int(0)
-  segundos = int(0)
   conteo = int(0)
-      #COMPROBACION DE VALORES INICIALES+
-'''
-print (num1Raw)
-print (num2Raw)
-print (num1Procesado)
-print (num2Procesado)
-print (resultado)
-print (respuesta)
-print (correcto)
-print (incorrecto)
-print (aciertos)
-print (fallos)
-print (nota)
-print (inicioRango)
-print (finalRango)
-print (segundos)
-print ("FIN COMPROBACION VALORES INICIO")
-input ("kj")
-'''
+  segundos = int(0)
 #___FIN DECLARACIÓN DE VARIABLES
-# COMIENZO PARTE LÓGICA
-# ALEATORIEDAD
-  # Define rango
-print ("OBTENCIÓN DEL RANGO:\n")
-inicioRango = int(input("Inicio del rango de valores: "))
-finalRango = int(input("Final del rango de valores: "))
-print (f"El rango de valores es: [({inicioRango}),({finalRango})]\n")
-inicio = int(1)
 
-# print ("¿Que quieres entrenar?")
-    # Funcion elegir numeros aleatorios
+# COMIENZO PARTE LÓGICA
+
+# ALEATORIEDAD
+
+# Define rango
+print ("OBTENCIÓN DEL RANGO:\n")
+print ("Si se deja en valores nulos provocará un error")
+inicioRango = int(input("Inicio del rango de valores: "))
+finalRango = (int(input("Final del rango de valores: ")))
+
+print (f"El rango de valores es: [({inicioRango}),({finalRango})]\n")
+print ((30 * "-"),"\n")
+
 def numerosAleatorios(): # Funcion que da valor aleatorio a los operandos.
   global num1Raw
   global num2Raw
   global segundos
-  num1Raw = random.randint( inicioRango, finalRango )
-  num2Raw = random.randint( inicioRango, finalRango )
-if inicio == 1:
+  num1Raw = random.randint(inicioRango,finalRango )
+  num2Raw = random.randint(inicioRango,finalRango )
+
+while conteo <= 10:
   numerosAleatorios()
-
-
-while aciertos < 10:
   def suma(): #SUMA
     global conteo
     global resultado
     global respuesta
     global aciertos
     global fallos
+    print ("SUMA")
+    conteo += 1
     resultado = num1Raw + num2Raw
     respuesta = int(input (f"\t\n{(num1Raw)} + {(num2Raw)} = "))
-    if respuesta == resultado:
+    if resultado == respuesta:
       print (Fore.GREEN + "Correcto" + Fore.RESET)
       aciertos += 1
     else: 
       print (Fore.RED + "Incorrecto" + Fore.RESET)
       fallos += 1
   suma()
-
+  numerosAleatorios()
   def resta(): #RESTA
     global conteo
     global resultado
@@ -121,14 +99,14 @@ while aciertos < 10:
       print (Fore.RED + "Incorrecto" + Fore.RESET)
       fallos += 1
   resta()
-
+  numerosAleatorios()
   def multi(): #MULTIPLICACIÓN
     global conteo
     global resultado
     global respuesta
     global aciertos
     global fallos
-    print ("MULTI")
+    print ("MULTIPLICACIÓN")
     conteo += 1
     resultado = num1Raw * num2Raw
     respuesta = int(input (f"\t\n{(num1Raw)} x {(num2Raw)} = "))
@@ -140,34 +118,12 @@ while aciertos < 10:
       fallos += 1
   multi()
 
-while inicio == 1:
-  segundos += 1
-  time.sleep(1)
-nota = int(((aciertos - fallos) / conteo) * 10)
+nota = int((aciertos/conteo)*10)
 nota = format(nota, ".2f")
 print ("") #intro
-print ("Fin de la partida")
-print ("Aciertos:", aciertos)
+print ("Fin de la partida:")
+print ("\nAciertos:", aciertos)
 print ("Fallos:", fallos)
 print ("Nota:", nota)
-print ("Tiempo:", segundos, "seg.")
-
-# Intentar separar los def del bucle while:
-'''
-  while aciertos < 10:
-    suma()
-    resta()
-    multi()
-        o
-    random.choice (suma(), resta(), multi()) 
-iMPEDIR QUE CRASHEE SI EL USUARIO PONE RESULTADO 0
-'''
-
-
-# OBJETIVOS DE LA VERSION 1.0:
-    # Que te pregunte si quieres hacer sumas, restas o multiplicaiones.
-    # Que si sacas mas de un 5.0 se ponga en fondo verde, si no, rojo.
-    # El contador de tiempo, que funcione.
-    # Revisar las variables inicailes por si sobra alguna.
-    # En caso de números negativos, que los ponga entre parentesis.
-    # Redondee las operaciones de la forma más adecuada a la respuesta del usuario.
+print ("")
+exit()
